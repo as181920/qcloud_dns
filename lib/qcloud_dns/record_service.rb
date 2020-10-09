9a10178de6e65a8ef3edf6a1fa0b43a20edbd5e0
Andersen Fan <as181920@gmail.com>
2020-10-09 19:20:47 +0800

    Add list/modify for record

--- lib/qcloud_dns/record_service.rb
+++ lib/qcloud_dns/record_service.rb
@@ -9,5 +9,20 @@ module QcloudDns
     def create(params={})
       client.perform params.merge(Action: "RecordCreate")
     end
+
+    def list(params={})
+      client.perform params.merge(Action: "RecordList")
+    end
+
+    def modify(params={})
+      if params[:recordId].present?
+        client.perform params.merge(Action: "RecordModify")
+      elsif params[:subDomain].present? && params[:recordType].present?
+        record_id = JSON.load(list(params).body).dig("data", "records")&.detect{ |attrs| attrs["name"].eql?(params[:subDomain]) && attrs["type"].eql?(params[:recordType]) && attrs["status"].eql?("enabled") }&.dig("id")
+        client.perform params.merge(Action: "RecordModify", recordId: record_id)
+      else
+        raise "params not valid!"
+      end
+    end
   end
 end
